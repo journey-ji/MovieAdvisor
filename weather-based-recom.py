6a45b3ba41b649285f07777fa149af3110dc1058
journey-ji <journeyji95@gmail.com>
2022-12-04 23:10:23 +0900

    날씨단어데이터셋-눈관련단어추가및-불필요연산코드삭제

--- weather-based-recom.py
+++ weather-based-recom.py
@@ -21,18 +21,18 @@ indices = pd.Series(df2.index,index=df2['title']).drop_duplicates()
 
 
 
-### 평점 가중치 구하기
-C = df2['vote_average'].mean()
-m = df2['vote_count'].quantile(0.7) ## 상위 10%에 해당하는 평가수를 가진 영화만 검색
-q_movies = df2.copy().loc[df2['vote_count']>=m] # 상위 10% 영화만 담은 리스트 생성
+# ### 평점 가중치 구하기
+# C = df2['vote_average'].mean()
+# m = df2['vote_count'].quantile(0.7) ## 상위 10%에 해당하는 평가수를 가진 영화만 검색
+# q_movies = df2.copy().loc[df2['vote_count']>=m] # 상위 10% 영화만 담은 리스트 생성
 
-def weighted_rating(x,m=m,C=C):
-  v = x['vote_count']
-  R = x['vote_average']
-  return (v/(v+m)*R) + (m/(v+m)*C)
+# def weighted_rating(x,m=m,C=C):
+#   v = x['vote_count']
+#   R = x['vote_average']
+#   return (v/(v+m)*R) + (m/(v+m)*C)
 
-## 평점가중치가 적용된 영화리스트 ! (상위 10% 이상의 추천수를 받은 영화만 가지고 있음)
-q_movies['score'] = q_movies.apply(weighted_rating,axis=1)
+# ## 평점가중치가 적용된 영화리스트 ! (상위 10% 이상의 추천수를 받은 영화만 가지고 있음)
+# q_movies['score'] = q_movies.apply(weighted_rating,axis=1)
 
 
 ## 날씨 데이터셋
@@ -47,8 +47,10 @@ def get_recommendation2(weather,cosine_sim=cosine_sim):
 
   if weather in ['Rain','Mist','Haze']:
      total_overview = 'rain rainfall storm downpour wet rainstorm thunderstorm procipitation weather deluge shower cloudburst downfall thundershower drizzle mist spit sprinkle mizzle scud'
+  elif weather in ['Snow','blizzard']:
+    total_overview = 'snow blizzard snowfall powder snowflake'
   else:
-     total_overview = 'sun sulight daylight glare shine sunburst'
+    total_overview = 'sun sulight daylight glare shine sunburst'
   
   ## 날씨 관련 단어셋
   # 기준 : 맑고 시원한 산들바람이 붐, 핵더움, 비옴, 눈옴 
@@ -69,7 +71,6 @@ def get_recommendation2(weather,cosine_sim=cosine_sim):
   
   movie_indicies = [i[0] for i in total_score]
   print(df2[['id','title']].iloc[movie_indicies])
-  #print(q_movies[['id','title']].iloc[movie_indicies])
 if __name__ == '__main__':
   
   get_recommendation2(sys.argv[1])
